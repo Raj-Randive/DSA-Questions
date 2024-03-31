add69e06c7ed48ba57963f5c7088a32087182a00
Raj Randive <randiveraj96@gmail.com>
2024-03-31 18:45:19 +0530

    bit-manipulation-easy-question

---
+++ Bit_Manipulation/78_Subsets.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+  vector<vector<int>> subsets(vector<int> &nums) {
+
+    // Solution using bit manipulation
+    vector<vector<int>> ans;
+    int n = nums.size();
+    int subsets = 1 << n; // Total subsets - 2^n
+
+    for (int i = 0; i < subsets; i++) {
+      vector<int> temp;
+      for (int j = 0; j < n; j++) {
+        // check if the bit is set or not
+        if (i & (1 << j)) {
+          temp.push_back(nums[j]);
+        }
+      }
+      ans.push_back(temp);
+    }
+    return ans;
+  }
+};
+
+int main() {
+  Solution sol;
+  return 0;
+}
